78fff603749000832aba2686e696b4957b5a87e5
JavierMoon1026 <moonj@udesa.edu.ar>
2025-05-31 17:42:04 -0300

    .

--- ejercicio1/example/main.cpp
+++ ejercicio1/example/main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 int main() {
-    Posicion posicion(20.4f, 50.4f, 90.0f, 5.3f);
-    Presion presion(10.3f, 5.8f, 6.1f);
+    Posicion posicion(-34.6f, -58.4f, 950.0f, 5.3f);
+    Presion presion(101.3f, 5.8f, 6.1f);
 
     SaveFlightData datos(posicion, presion);
     datos.serializar("vuelo.dat");
